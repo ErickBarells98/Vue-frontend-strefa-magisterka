db1f7d6d9fa356094dabe5a8477553a2d5ec5a71
Eryk Beczek <erykos9898@wp.pl>
2022-08-31 16:56:48 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+      </nav>
+  </header>
+
+  <div class="main-content">
+    <RouterView />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
